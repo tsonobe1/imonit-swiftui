8343e03c61cddf011a432c1ea958b0a517db8118
tsonobe <sono.tak.st@gmail.com>
2023-08-14 21:34:08 +0900

    Initial Commit

---
+++ imonit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  imonit
+//
+//  Created by 薗部拓人 on 2023/08/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ imonit/imonitApp.swift
@@ -0,0 +1,17 @@
+//
+//  imonitApp.swift
+//  imonit
+//
+//  Created by 薗部拓人 on 2023/08/14.
+//
+
+import SwiftUI
+
+@main
+struct imonitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
